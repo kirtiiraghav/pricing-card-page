4f3daeaa14340d7af00973560ac1620ef59ffe83
kirti <kirti.20cs@saividya.ac.in>
2024-07-15 11:10:19 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens:{
+        'media980': '980px'
+      }
+    },
+  },
+  plugins: [],
+}
+
